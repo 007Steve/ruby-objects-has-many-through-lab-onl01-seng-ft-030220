dd74979abc730dbb63199cff3c2cc36e9446f4d7
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-03-27 18:52:35 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist 
+  attr_accessor :name,:songs
+  @@all  = []
+  def initialize(name)
+  @name = name
+  #@songs = songs
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+def songs
+  Song.all.select {|song| song.artist == self}
+end
+
+def new_song(song_name,genre)
+  Song.new(song_name,self,genre)
+end
+
+def genres
+ self.songs.map{|song| song.genre}
+end
+
+end 
